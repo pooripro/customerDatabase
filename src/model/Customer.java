7a6d74e6d3fadfcae8e5982d8bf82c63db1213ab
pooripro <steampro35200@gmail.com>
2020-05-10 16:34:30 +0700

    <ADDED> model.customer

---
+++ src/model/Customer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author pooripro
+ */
+public class Customer {
+    private String firstName;
+    private String lastName;
+    
+    public Customer(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    public String getFirstName(){
+        return firstName;
+    }
+    
+    public String getLastName(){
+        return lastName;
+    }
+}
